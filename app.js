2d1ba0b606fc457222913c854122c900cdaeab03
sagar mourya <mourya13sagar@gmail.com>
2024-08-14 15:49:02 +0530

    ep-2

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 
-const heading= React.createElement("h1",{ id:"heading",xyz:"abc"},"hello bhaiyo")
+const heading= React.createElement("div",{ id:"heading"},[
+    React.createElement("div",{ id:"child"},[
+        React.createElement("h1",{},"I am an h1 tag for child"),
+        React.createElement("h2",{},"I am an h2 tag for child"),
+    ]),
+    React.createElement("div",{ id:"child2"},[
+        React.createElement("h1",{},"I am an h1 tag for child2"),
+        React.createElement("h2",{},"I am an h2 tag for child2"),
+    ]),
+]);
 const root=ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
